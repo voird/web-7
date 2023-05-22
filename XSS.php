fbf60f7e3b7146858ad676addd54171fd5627a1e
polina_storozh <58592174+voird@users.noreply.github.com>
2023-05-22 08:37:18 +0300

    Create XSS.php

---
+++ XSS.php
@@ -0,0 +1,9 @@
+$fio = strip_tags($_POST['fio']);
+$email = strip_tags($_POST['email']);
+$year = intval($_POST['year']);
+$gender = strip_tags($_POST['gender']);
+$gender = strip_tags($_POST['gender']);
+$message = strip_tags($_POST['text']);
+$message = strip_tags($_POST['superpower']);
+$message = strip_tags($_POST['limbs']);
+
