166cab79774e7e9e6835734572ba2cf55af6905a
polina_storozh <58592174+voird@users.noreply.github.com>
2023-05-22 09:05:41 +0300

    Update XSS.php

--- XSS.php
+++ XSS.php
@@ -1,9 +1,17 @@
+// Санитизация
+
 $fio = strip_tags($_POST['fio']);
 $email = strip_tags($_POST['email']);
 $year = intval($_POST['year']);
 $gender = strip_tags($_POST['gender']);
 $gender = strip_tags($_POST['gender']);
 $message = strip_tags($_POST['text']);
-$message = strip_tags($_POST['superpower']);
-$message = strip_tags($_POST['limbs']);
+$spwr = strip_tags($_POST['superpower']);
+$limbs = strip_tags($_POST['limbs']);
 
+// Пример проверка на соответствие формату
+
+if (!preg_match("/^[\w\.-]+@([\w-]+\.)+[\w-]{2,4}$/", $_POST['email']) || empty($_POST['email'])) {
+  print('Заполните email.<br/>');
+  $errors = TRUE;
+}
